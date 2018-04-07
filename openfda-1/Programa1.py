019da966eaa1cee80210d69819a410ef15179cea
guillesg <guillermo.saez@hotmail.es>
2018-04-07 23:00:08 +0200

    Programa 1 de openfda-1

--- openfda-1/Programa1.py
+++ openfda-1/Programa1.py
@@ -8,20 +8,24 @@ connect = http.client.HTTPSConnection("api.fda.gov")
 # Hace una solicitud mediante el verbo "GET" concretando el recurso desde donde obtendremos la información
 connect.request("GET", "/drug/label.json", None, headers)
 # Leemos la información que nos responde el servidor
-r1 = connect.getresponse()
+response1 = connect.getresponse()
 # Imprimimos por pantalla línea de estado de la respuesta
-print(r1.status, r1.reason)
-# Se lee la respuesta recibido y la descodifica convirtiéndolo en un string
-r1 = r1.read().decode("utf-8")
+print(response1.status, response1.reason)
+# Se lee la respuesta recibida y la descodifica desde utf-8 al formato local
+response1 = response1.read().decode("utf-8")
 # Lo pasa a diccionario
-drugs = json.loads(r1)
+drugs = json.loads(response1)
 
+# Creamos un diccionario vacío
+info = {}
+# Vamos añadiendo claves con sus respectivos valores obtenidos del diccionario de la respuesta del servidor
+info["id"] = drugs["results"][0]["id"]
+info["propósito"] = drugs["results"][0]["purpose"][0]
+info["fabricante"] = drugs["results"][0]["openfda"]["manufacturer_name"][0]
 
-dicc = {}
-dicc["id"] = drugs["results"][0]["id"]
-dicc["propósito"] = drugs["results"][0]["purpose"][0]
-dicc["fabricante"] = drugs["results"][0]["openfda"]["manufacturer_name"][0]
+# Imprimimos por pantalla las claves con sus valores asociados
+for feature,answer in info.items():
+    print("El {} del medicamento es: {}".format(feature, answer))
 
-for feature,solution in dicc.items():
-    print("El {} del medicamento es: {}".format(feature, solution))
+# Finalmente cerramos la conexión
 connect.close()
