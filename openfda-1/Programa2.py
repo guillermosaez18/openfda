e50db31207f408ad84e7333855529a729b80e55d
guillesg <guillermo.saez@hotmail.es>
2018-04-07 23:17:42 +0200

    Programa 2 de openfda-1

--- openfda-1/Programa2.py
+++ openfda-1/Programa2.py
@@ -1,17 +1,27 @@
 import http.client
 import json
-
+# Escribimos la cabecera del mensaje de solicitud
 headers = {'User-Agent': 'http-client'}
+# Declaramos el número de medicamentos que deseamos ver
 n = 10
 
-conn = http.client.HTTPSConnection("api.fda.gov")
-conn.request("GET", "/drug/label.json?limit={}".format(n), None, headers)
-r2 = conn.getresponse()
-print(r2.status, r2.reason)
-# Se lee el archivo json recibido y se pasa a string
-r2 = r2.read().decode("utf-8")
+# Se conecta a la API general de FDA, al servidor
+connect = http.client.HTTPSConnection("api.fda.gov")
+# Hace una solicitud mediante el verbo "GET" concretando el recurso desde donde obtendremos la información
+# Mediante el parámetro limit=n hacemos posible ver la información de n medicamentos
+connect.request("GET", "/drug/label.json?limit={}".format(n), None, headers)
+# Leemos la información que nos responde el servidor
+response2 = connect.getresponse()
+# Imprimimos por pantalla línea de estado de la respuesta
+print(response2.status, response2.reason)
+# Se lee la respuesta recibida y la descodifica desde utf-8 al formato local
+response2 = response2.read().decode("utf-8")
 # Lo pasa a diccionario
-drugs = json.loads(r2)
+drugs = json.loads(response2)
 
+# Vamos pasando por los medicamentos en orden y extrayendo la información que queremos (del diccionario)
 for num in range(n):
     print ("El medicamento número {} tiene id: {}".format(num+1, drugs["results"][num]["id"]))
+
+# Finalmente cerramos la conexión
+connect.close()
