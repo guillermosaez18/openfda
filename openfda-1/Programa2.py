d149ad9c333bd956ee0d9f1e8a2f9efc2757e347
guillesg <guillermo.saez@hotmail.es>
2018-04-08 13:43:12 +0200

    Programas de openfda-1

--- openfda-1/Programa2.py
+++ openfda-1/Programa2.py
@@ -1,5 +1,6 @@
 import http.client
 import json
+
 # Escribimos la cabecera del mensaje de solicitud
 headers = {'User-Agent': 'http-client'}
 # Declaramos el número de medicamentos que deseamos ver
@@ -16,7 +17,7 @@ response2 = connect.getresponse()
 print(response2.status, response2.reason)
 # Se lee la respuesta recibida y la descodifica desde utf-8 al formato local
 response2 = response2.read().decode("utf-8")
-# Lo pasa a diccionario 
+# Lo pasa a diccionario
 drugs = json.loads(response2)
 
 # Vamos pasando por los medicamentos en orden y extrayendo la información que queremos (del diccionario)
